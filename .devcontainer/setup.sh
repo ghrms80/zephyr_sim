30a5c770329a792842fba36beba9d3b0a54817ac
jiho <ghrms80@gmail.com>
2026-05-03 16:57:35 +0900

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,7 +4,7 @@ set -e
 echo "Starting Zephyr environment setup..."
 
 # 1. West update (소스 코드 다운로드)
-west update --narrow --depth 1
+west update --narrow --depth 1 --no-history
 
 # 2. Python 종로성 설치 (필요시)
 pip install --user -r zephyr/scripts/requirements.txt
